c900a654eb3c249889d36cfb0626990dcfaa8262
MichaelWebHub <michael.kutateladze@yandex.ru>
2018-04-02 20:33:36 +0300

    Preparations

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ module.exports = {
     username: "heroku_619cjc94",
     password: "1256330_Mongo",
     //mongodbUri: "mongodb://heroku_619cjc94:1256330_Mongo@ds125195.mlab.com:25195/heroku_xbfdqzgp",
-    uri: "mongodb://heroku_619cjc94:1256330_Mongo@ds231739.mlab.com:31739/heroku_619cjc94",
+    uri: "mongodb://michaeladze:1256330_Mongo@ds231739.mlab.com:31739/heroku_619cjc94",
     localUri: "mongodb://localhost/petrostage"
 };
